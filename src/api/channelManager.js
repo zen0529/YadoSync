1c5dba91436db5262231e85547de81c1309a0e20
aldren <aldrenarcenal.aa@gmail.com>
2026-08-18 18:49:02 +0800

    KAN 27 - Remove files not connected to the project

--- src/api/channelManager.js
+++
@@ -1,6 +0,0 @@
-import { beds24Adapter } from './adapters/beds24Adapter'
-
-// To switch to Channex in the future, change this one line:
-// import { channexAdapter } from './adapters/channexAdapter'
-
-export const channelManager = beds24Adapter
